c83b9298d1242ebddb22ed9c773477cafb05efd1
Dominik <dominikjaniga91@gmail.com>
2020-05-19 13:24:09 +0200

    Create address entity

---
+++ src/main/java/com/cvgenerator/cvgenerator/domain/entity/Address.java
@@ -0,0 +1,27 @@
+package com.cvgenerator.cvgenerator.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private int houseNumber;
+    private String zipCode;
+    private String city;
+
+}
