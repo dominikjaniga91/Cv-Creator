60d1bc3ea6bdddb04227e16354de6b81631bfb3b
Dominik <dominikjaniga91@gmail.com>
2020-05-19 23:50:03 +0200

    Create service & repository for user

---
+++ src/main/java/com/cvgenerator/cvgenerator/service/UserService.java
@@ -0,0 +1,9 @@
+package com.cvgenerator.cvgenerator.service;
+
+
+import com.cvgenerator.cvgenerator.domain.entity.User;
+
+public interface UserService {
+
+    User saveUser(User user);
+}
---
+++ src/main/java/com/cvgenerator/cvgenerator/service/implementation/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.cvgenerator.cvgenerator.service.implementation;
+
+import com.cvgenerator.cvgenerator.domain.entity.User;
+import com.cvgenerator.cvgenerator.repository.UserRepository;
+import com.cvgenerator.cvgenerator.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+}
