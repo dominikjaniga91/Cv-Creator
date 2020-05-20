3896fea1c4af45dbf5e398d7e6adcfd0bd1ec179
Dominik <dominikjaniga91@gmail.com>
2020-05-20 21:21:25 +0200

    Create AuthenticationFilter with doFilter
    Method pass request to authentication service.
    After service check request token return authentication
    which is saving in SecurityContext.

---
+++ src/main/java/com/cvgenerator/config/filter/AuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.cvgenerator.config.filter;
+
+import com.cvgenerator.config.security.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationFilter(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        Authentication authentication = authenticationService.getAuthentication(request);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+
+    }
+}
