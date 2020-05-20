5eca346b25337a347d159ba249dcabd2262c7ed6
Dominik <dominikjaniga91@gmail.com>
2020-05-20 22:33:28 +0200

    Add sucessfulAuthentication method in LoginFilter

--- src/main/java/com/cvgenerator/config/filter/LoginFilter.java
+++ src/main/java/com/cvgenerator/config/filter/LoginFilter.java
@@ -1,34 +1,48 @@
 package com.cvgenerator.config.filter;
 
+import com.cvgenerator.config.security.AuthenticationService;
 import com.cvgenerator.domain.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.security.authentication.AuthenticationManager;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Collections;
 
-public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+@Component
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
-    public LoginFilter(String url, AuthenticationManager authenticationManager) {
-        super(new AntPathRequestMatcher(url));
-        setAuthenticationManager(authenticationManager);
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    public LoginFilter(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
     }
 
+    @SneakyThrows
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws AuthenticationException, IOException {
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
         User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
         UsernamePasswordAuthenticationToken token =  new UsernamePasswordAuthenticationToken(user.getEmail(),
-                                                                                             user.getPassword(),
-                                                                                             Collections.emptyList());
+                user.getPassword(),
+                Collections.emptyList());
         return getAuthenticationManager().authenticate(token);
+    }
 
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult){
+
+        User user = (User)authResult.getPrincipal();
+        authenticationService.createToken(user, response);
     }
 }
