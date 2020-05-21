20439276b87df31f4aa656c13d73cc58de82b03f
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-22 00:27:38 +0200

    Create user dto class

---
+++ src/main/java/com/cvgenerator/domain/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.cvgenerator.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
+    private String password;
+    private boolean isActive;
+    private LocalDateTime registration;
+}
