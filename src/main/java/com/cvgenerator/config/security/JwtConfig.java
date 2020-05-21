80d8afe3c37461623a94db49813af6d582349b83
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-21 19:18:56 +0200

    Add @Component annotation in JwtConfig

--- src/main/java/com/cvgenerator/config/security/JwtConfig.java
+++ src/main/java/com/cvgenerator/config/security/JwtConfig.java
@@ -2,8 +2,10 @@ package com.cvgenerator.config.security;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class JwtConfig {
 
     @Value("${jwt.secret}")
@@ -15,10 +17,10 @@ public class JwtConfig {
     @Value("${jwt.prefix}")
     private String prefix;
 
-    @Value(("${jwt.header}"))
+    @Value("${jwt.header}")
     private String header;
 
-    @Value(("${jwt.additionalHeader}"))
+    @Value("${jwt.additionalHeader}")
     private String additionalHeader;
 
 
