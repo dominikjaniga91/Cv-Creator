d86cf92f62f14edefcc9b09747e3df388b350ad0
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-21 19:33:45 +0200

    Override to string in language enum

--- src/main/java/com/cvgenerator/domain/enums/LanguageLevel.java
+++ src/main/java/com/cvgenerator/domain/enums/LanguageLevel.java
@@ -2,5 +2,22 @@ package com.cvgenerator.domain.enums;
 
 public enum LanguageLevel {
 
-    A1, A2, B1, B2, C1, C2, NATIVE
+    A1("A1"),
+    A2("A2"),
+    B1("B1"),
+    B2("B2"),
+    C1("C1"),
+    C2("C2"),
+    NATIVE("Native");
+
+    private String value;
+
+    LanguageLevel(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
