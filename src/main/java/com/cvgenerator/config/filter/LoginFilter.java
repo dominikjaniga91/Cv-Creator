15a5cf48881795c8d33c5290426fe6d61164b134
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-21 15:18:05 +0200

    Change superclass for LoginFilter
    Previous UsernamePasswordAuthenticationFilter was
    replaced to AbstractAuthenticationProcessingFilter
    because of problems with AuthenticationManager bean

--- src/main/java/com/cvgenerator/config/filter/LoginFilter.java
+++ src/main/java/com/cvgenerator/config/filter/LoginFilter.java
@@ -5,24 +5,28 @@ import com.cvgenerator.domain.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Collections;
 
-@Component
-public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
-    private AuthenticationService authenticationService;
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 
-    @Autowired
-    public LoginFilter(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
+    private AuthenticationService authenticationService = new AuthenticationService();
+
+    public LoginFilter(String url, AuthenticationManager authenticationManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authenticationManager);
     }
 
     @SneakyThrows
