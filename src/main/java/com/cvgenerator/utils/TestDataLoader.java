12d9e35b750dc473e46638a60a0a0fee9109ea13
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-21 17:12:32 +0200

    Add additional data to testDateLoader

--- src/main/java/com/cvgenerator/utils/TestDataLoader.java
+++ src/main/java/com/cvgenerator/utils/TestDataLoader.java
@@ -1,13 +1,11 @@
 package com.cvgenerator.utils;
 
+import com.cvgenerator.domain.entity.*;
 import com.cvgenerator.repository.*;
-import com.cvgenerator.domain.entity.Address;
-import com.cvgenerator.domain.entity.PersonalData;
-import com.cvgenerator.domain.entity.User;
-import com.cvgenerator.domain.entity.UserCv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
@@ -54,11 +52,11 @@ public class TestDataLoader {
     @PostConstruct
     public void saveUsersToDatabase(){
 
-        User user1 = createUser("Dominik", "Janiga", "dominikjaniga@gmail.com", "dominik123", true, LocalDateTime.now());
-        User user2 = createUser("Andrzej", "Kowalski", "andrezjkowalski@gmail.com", "andrej123", true, LocalDateTime.now());
-        User user3 = createUser("Jurek", "Nowak", "jureknowak@gmail.com", "jurek123", true, LocalDateTime.now());
-        User user4 = createUser("Katarzyna", "Piekarska", "katarzynapiekarska@gmail.com", "kasia123", true, LocalDateTime.now());
-        User user5 = createUser("Anna", "Pompka", "annapompka@gmail.com", "anna123", false, LocalDateTime.now());
+        User user1 = createUser("Dominik", "Janiga", "dominikjaniga@gmail.com", "dominik123", true, "ROLE_USER", LocalDateTime.now());
+        User user2 = createUser("Andrzej", "Kowalski", "andrezjkowalski@gmail.com", "andrej123", true, "ROLE_USER", LocalDateTime.now());
+        User user3 = createUser("Jurek", "Nowak", "jureknowak@gmail.com", "jurek123", true, "ROLE_USER", LocalDateTime.now());
+        User user4 = createUser("Katarzyna", "Piekarska", "katarzynapiekarska@gmail.com", "kasia123", true, "ROLE_USER", LocalDateTime.now());
+        User user5 = createUser("Anna", "Pompka", "annapompka@gmail.com", "anna123", false, "ROLE_USER", LocalDateTime.now());
 
         Address address1 = createAddress("Źródlana", 56, "33-111", "Koszyce Male");
         Address address2 = createAddress("Bitwy pod Lenino", 24, "80-809", "Gdansk");
@@ -78,21 +76,179 @@ public class TestDataLoader {
         UserCv cv4 = createUserCv("cv4", "premium", " summary template4", " RODO clause4", user4, personalData4);
         UserCv cv5 = createUserCv("cv5", "semantic", " summary template5", " RODO clause5", user5, personalData5);
 
+        createCourse("Altkom Akademia", "Kraków", LocalDate.of(2020,1,1), LocalDate.of(2020,6,30), "Java programming", cv1);
+        createCourse("Kodilla", "Online", LocalDate.of(2020,1,1), LocalDate.of(2020,6,30), "JavaScript programming", cv2);
+        createCourse("SDA", "Warszawa", LocalDate.of(2020,1,1), LocalDate.of(2020,6,30), "Python programming", cv3);
+        createCourse("Altkom Akademia", "Poznań", LocalDate.of(2020,1,1), LocalDate.of(2020,6,30), "Front-end", cv4);
+        createCourse("Coders Lab", "Kraków", LocalDate.of(2020,1,1), LocalDate.of(2020,6,30), "Node.JS programming", cv5);
+
+        createEducation("AGH", "Kraków", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "Master Degree", "Elektrotechnika", null, cv1);
+        createEducation("Polotechnika Wrocławska", "Wrocaław", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "Master Degree", "Automatyka", null, cv2);
+        createEducation("AGH", "Kraków", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "Master Degree", "Geologia", null, cv3);
+        createEducation("Politechnika Warszawska", "Warszawa", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "Master Degree", "Elektrotechnika", null, cv4);
+        createEducation("AGH", "Kraków", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "Master Degree", "Geofizyka", null, cv5);
+
+
+        createExperience("Jeronimo Martins", "Kraków", "Magazynier", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), null, cv1);
+        createExperience("Lpp", "Kraków", "Kontroller", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), null, cv2);
+        createExperience("Uber", "Kraków", "Kierowca", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), null, cv3);
+        createExperience("McDonaclds", "Kraków", "Sprzedawca", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), null, cv4);
+        createExperience("Jeronimo Martins", "Kraków", "Sprzedawca", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), null, cv5);
+
+        createInterest("Wspinaczka, Piwo, Koszykówka ", cv1);
+        createInterest("Piłka nożna, Piwo, Koszykówka ", cv2);
+        createInterest("Kolarstwo, Piwo, Netflix ", cv3);
+        createInterest("Komputery, Piwo, Czytanie książek ", cv4);
+        createInterest("Siatkówka, Piwo, Koszykówka ", cv5);
+
+        createLanguage("English", null, "B2", 3, cv1);
+        createLanguage("Germany", null, "B1", 3, cv2);
+        createLanguage("French", null, "C1", 4, cv3);
+        createLanguage("English", null, "C2", 5, cv4);
+        createLanguage("Germany", null, "B2", 3, cv5);
+
+        createProject("Car Database", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "Simple CRUD app", cv1);
+        createProject("Flights Database", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "Simple CRUD app", cv2);
+        createProject("Konkurencja CROPP", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "Simple CRUD app", cv3);
+        createProject("Battleship game ", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "GRa w statki", cv4);
+        createProject("Car Database", LocalDate.of(2015,1,1), LocalDate.of(2020,6,30), "Simple CRUD app", cv5);
+
+
+        createSkills("Samoorganizacja", null, "Zaawansowany", 4, cv1);
+        createSkills("Java", null, "Expert", 5, cv2);
+        createSkills("Spring", null, "Zaawansowany", 4, cv3);
+        createSkills("SQL", null, "Zaawansowany", 4, cv4);
+        createSkills("GIT", null, "Zaawansowany", 4, cv5);
+
     }
+
+    private void createCourse(String school,
+                              String city,
+                              LocalDate startDate,
+                              LocalDate finishDate,
+                              String despription,
+                              UserCv cv) {
+        Course course = new Course();
+        course.setSchool(school);
+        course.setCity(city);
+        course.setStartDate(startDate);
+        course.setFinishDate(finishDate);
+        course.setDescription(despription);
+        course.setUserCv(cv);
+        courseRepository.save(course);
+    }
+
+    private void createEducation(String school,
+                                String city,
+                                LocalDate startDate,
+                                LocalDate finishDate,
+                                String degree,
+                                String specialization,
+                                String description,
+                                UserCv cv){
+
+        Education education = new Education();
+        education.setSchool(school);
+        education.setCity(city);
+        education.setStartDate(startDate);
+        education.setFinishDate(finishDate);
+        education.setDegree(degree);
+        education.setSpecialization(specialization);
+        education.setDescription(description);
+        education.setUserCv(cv);
+        educationRepository.save(education);
+    }
+
+
+    private void createExperience(String company,
+                                 String city,
+                                 String position,
+                                 LocalDate startDate,
+                                 LocalDate finishDate,
+                                 String description,
+                                 UserCv cv){
+
+        Experience experience = new Experience();
+        experience.setCompany(company);
+        experience.setCity(city);
+        experience.setPosition(position);
+        experience.setStartDate(startDate);
+        experience.setFinishDate(finishDate);
+        experience.setDescription(description);
+        experience.setUserCv(cv);
+        experienceRepository.save(experience);
+    }
+
+    private void createInterest(String name, UserCv cv){
+
+        Interest interest = new Interest();
+        interest.setName(name);
+        interest.setUserCv(cv);
+        interestRepository.save(interest);
+
+    }
+
+    private void createLanguage(String name,
+                                String description,
+                                String level,
+                                Integer stars,
+                                UserCv cv){
+        Language language = new Language();
+        language.setName(name);
+        language.setDescription(description);
+        language.setLevel(level);
+        language.setStars(stars);
+        language.setUserCv(cv);
+        languageRepository.save(language);
+
+    }
+
+    private void createProject(String name,
+                                LocalDate startDate,
+                                LocalDate finishDate,
+                                String description,
+                                UserCv cv){
+
+        Project project = new Project();
+        project.setName(name);
+        project.setStartDate(startDate);
+        project.setFinishDate(finishDate);
+        project.setDescription(description);
+        project.setUserCv(cv);
+        projectRepository.save(project);
+    }
+
+    private void createSkills(String name,
+                            String description,
+                            String level,
+                            Integer stars,
+                              UserCv cv){
+
+        Skill skill = new Skill();
+        skill.setName(name);
+        skill.setDescription(description);
+        skill.setLevel(level);
+        skill.setStars(stars);
+        skill.setUserCv(cv);
+        skillRepository.save(skill);
+    }
+
 
     private User createUser(String firstName,
                              String lastName,
                              String email,
                              String password,
                              boolean isActive,
+                             String role,
                              LocalDateTime registration){
-        User user  = new User();
 
+        User user  = new User();
         user.setFirstName(firstName);
         user.setLastName(lastName);
         user.setEmail(email);
         user.setPassword(password);
         user.setActive(isActive);
+        user.setRole(role);
         user.setRegistration(registration);
         userRepository.save(user);
         return user;
