9127bef26d06b100ca6c33d35c84a5832e7bf394
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-22 13:58:28 +0200

    Create PersonalDataDto class

---
+++ src/main/java/com/cvgenerator/domain/dto/PersonalDataDto.java
@@ -0,0 +1,20 @@
+package com.cvgenerator.domain.dto;
+
+import com.cvgenerator.domain.entity.Address;
+
+public class PersonalDataDto {
+
+    private Long id;
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String profession;
+    private String linkedIn;
+    private String github;
+    private String twitter;
+    private String website;
+    private Address address;
+
+}
