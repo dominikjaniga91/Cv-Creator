12def05f2f8fdd6ece0304f6998941d425855d02
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-22 13:55:20 +0200

    Create ProjectDto class

---
+++ src/main/java/com/cvgenerator/domain/dto/ProjectDto.java
@@ -0,0 +1,12 @@
+package com.cvgenerator.domain.dto;
+
+import java.time.LocalDate;
+
+public class ProjectDto {
+
+    private Long id;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate finishDate;
+    private String description;
+}
