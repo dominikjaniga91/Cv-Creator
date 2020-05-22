b1cbc8720458a5f9760819b4f845035b81f62a70
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-22 14:00:36 +0200

    Create courseDto class

---
+++ src/main/java/com/cvgenerator/domain/dto/CourseDto.java
@@ -0,0 +1,14 @@
+package com.cvgenerator.domain.dto;
+
+import java.time.LocalDate;
+
+public class CourseDto {
+
+    private Long id;
+    private String school;
+    private String city;
+    private LocalDate startDate;
+    private LocalDate finishDate;
+    private String description;
+
+}
