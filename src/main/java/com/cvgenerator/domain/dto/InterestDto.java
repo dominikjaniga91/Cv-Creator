d111d91bba3e95b12f4a0d6b6fb453cdefc1b324
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-22 14:07:39 +0200

    Create InterestDto class

---
+++ src/main/java/com/cvgenerator/domain/dto/InterestDto.java
@@ -0,0 +1,16 @@
+package com.cvgenerator.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InterestDto {
+
+    private Long id;
+    private String name;
+}
