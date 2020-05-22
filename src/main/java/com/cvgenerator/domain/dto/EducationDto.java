d64433b8511d334422214b5a2c1f29bd26bc6544
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-22 14:02:28 +0200

    Create educationDto class

---
+++ src/main/java/com/cvgenerator/domain/dto/EducationDto.java
@@ -0,0 +1,26 @@
+package com.cvgenerator.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EducationDto {
+
+    private Long id;
+    private String school;
+    private String city;
+    private LocalDate startDate;
+    private LocalDate finishDate;
+    private String degree;
+    private String specialization;
+    private String description;
+
+}
