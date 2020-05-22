a3a4537928464525615d81d973dfd0aaf4ad588b
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-22 14:03:29 +0200

    Add lombok annotations to dto classes

--- src/main/java/com/cvgenerator/domain/dto/CourseDto.java
+++ src/main/java/com/cvgenerator/domain/dto/CourseDto.java
@@ -1,7 +1,15 @@
 package com.cvgenerator.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourseDto {
 
     private Long id;
--- src/main/java/com/cvgenerator/domain/dto/PersonalDataDto.java
+++ src/main/java/com/cvgenerator/domain/dto/PersonalDataDto.java
@@ -1,7 +1,15 @@
 package com.cvgenerator.domain.dto;
 
 import com.cvgenerator.domain.entity.Address;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonalDataDto {
 
     private Long id;
