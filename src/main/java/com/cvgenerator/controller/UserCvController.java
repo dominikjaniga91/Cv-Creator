07ddc689b208bff824c7ca68fdb955ab7dd60fa7
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-23 10:45:59 +0200

    Add post mapping for save user cv

--- src/main/java/com/cvgenerator/controller/UserCvController.java
+++ src/main/java/com/cvgenerator/controller/UserCvController.java
@@ -5,14 +5,12 @@ import com.cvgenerator.service.implementation.UserCvServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class UserCvController {
+
 
     private UserCvServiceImpl userCvService;
 
@@ -26,5 +24,12 @@ public class UserCvController {
 
         return new ResponseEntity<>(userCvService.getUserCvById(id), HttpStatus.OK);
     }
+
+    @PostMapping("/cv/{userId}")
+    public void saveUserCv(@PathVariable Long userId, @RequestBody UserCvDto userCvDto){
+        userCvService.saveUserCv(userId, userCvDto);
+    }
+
+
 
 }
