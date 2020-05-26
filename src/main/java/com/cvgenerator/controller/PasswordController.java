83633161ac4c012dddc43b092d6176355dcb5ff1
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-27 01:26:13 +0200

    Add request for password reset in PasswordControl

--- src/main/java/com/cvgenerator/controller/PasswordController.java
+++ src/main/java/com/cvgenerator/controller/PasswordController.java
@@ -2,8 +2,10 @@ package com.cvgenerator.controller;
 
 import com.cvgenerator.domain.entity.Token;
 import com.cvgenerator.domain.entity.User;
+import com.cvgenerator.repository.UserRepository;
 import com.cvgenerator.service.implementation.TokenServiceImpl;
 import com.cvgenerator.service.implementation.UserServiceImpl;
+import com.cvgenerator.utils.service.implementation.MailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,15 +17,28 @@ public class PasswordController {
 
     private final TokenServiceImpl tokenService;
     private final UserServiceImpl userService;
+    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final MailServiceImpl mailService;
 
     @Autowired
     public PasswordController(TokenServiceImpl tokenService,
                               UserServiceImpl userService,
-                              PasswordEncoder passwordEncoder) {
+                              PasswordEncoder passwordEncoder,
+                              MailServiceImpl mailService,
+                              UserRepository userRepository) {
         this.tokenService = tokenService;
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.mailService = mailService;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/password-reset/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void requestForPasswordReset(@PathVariable Long userId){
+        User user = userRepository.findById(userId).orElseThrow();
+        mailService.sendPasswordResetEmail(user);
     }
 
     @PostMapping("/new-password")
