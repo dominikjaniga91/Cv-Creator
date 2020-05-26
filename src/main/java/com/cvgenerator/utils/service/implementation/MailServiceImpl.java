dc6a69ce4f8af9b418c1005975b11b25c495f93e
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-26 20:44:59 +0200

    Add message from properties file in mail service

--- src/main/java/com/cvgenerator/utils/service/implementation/MailServiceImpl.java
+++ src/main/java/com/cvgenerator/utils/service/implementation/MailServiceImpl.java
@@ -1,27 +1,35 @@
 package com.cvgenerator.utils.service.implementation;
 
+import com.cvgenerator.config.MailMessageConfig;
 import com.cvgenerator.utils.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class MailServiceImpl implements MailService {
 
     private final JavaMailSender javaMailSender;
+    private final MailMessageConfig mail;
 
     @Autowired
-    MailServiceImpl(JavaMailSender javaMailSender) {
+    MailServiceImpl(JavaMailSender javaMailSender, MailMessageConfig mail) {
         this.javaMailSender = javaMailSender;
+        this.mail = mail;
     }
 
     @Override
     public void sendConfirmationEmail() {
+        String token = UUID.randomUUID().toString();
+        String message = mail.getConfirmationMessage() + "\n" + mail.getConfirmationLink() + token;
+
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo("dominikjaniga91@gmail.com");;
-        mailMessage.setSubject("Confirmation email");
-        mailMessage.setText("Please click to below link to confirm email ");
+        mailMessage.setSubject(mail.getConfirmationTitle());
+        mailMessage.setText(message);
         javaMailSender.send(mailMessage);
     }
 }
