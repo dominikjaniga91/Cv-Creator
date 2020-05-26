79971b0ff5dedc6376422dbc9b2b9bb14047b905
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-05-26 20:41:07 +0200

    Add property source to mail message config

--- src/main/java/com/cvgenerator/config/MailMessageConfig.java
+++ src/main/java/com/cvgenerator/config/MailMessageConfig.java
@@ -2,10 +2,12 @@ package com.cvgenerator.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
+@PropertySource("classpath:mail.message.properties")
 public class MailMessageConfig {
 
     @Value("${mail.confirmation.title}")
