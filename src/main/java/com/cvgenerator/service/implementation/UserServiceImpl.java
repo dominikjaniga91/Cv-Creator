55e6062e10f5b1586bc821e68c644f8aa5e2d207
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-07 20:40:02 +0200

    Change saveUser method in UserServiceImpl
    Encode new user password before saving user to DB
    and add method that send confirmation email for new
    user.

--- src/main/java/com/cvgenerator/service/implementation/UserServiceImpl.java
+++ src/main/java/com/cvgenerator/service/implementation/UserServiceImpl.java
@@ -9,11 +9,12 @@ import com.cvgenerator.repository.UserRepository;
 import com.cvgenerator.service.UserService;
 import com.cvgenerator.service.dtoConverters.UserCvShortDtoConverter;
 import com.cvgenerator.service.dtoConverters.UserDtoConverter;
+import com.cvgenerator.utils.service.implementation.MailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -23,24 +24,30 @@ public class UserServiceImpl implements UserService {
     private PasswordEncoder passwordEncoder;
     private UserDtoConverter userDtoConverter;
     private UserCvShortDtoConverter userCvShortDtoConverter;
+    private final MailServiceImpl mailService;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository,
                            UserCvRepository userCvRepository,
                            PasswordEncoder passwordEncoder,
                            UserDtoConverter userDtoConverter,
-                           UserCvShortDtoConverter userCvShortDtoConverter){
+                           UserCvShortDtoConverter userCvShortDtoConverter,
+                           MailServiceImpl mailService){
 
         this.userRepository = userRepository;
         this.userCvRepository = userCvRepository;
         this.passwordEncoder = passwordEncoder;
         this.userDtoConverter = userDtoConverter;
         this.userCvShortDtoConverter = userCvShortDtoConverter;
+        this.mailService = mailService;
     }
 
     @Override
     public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRegistration(LocalDateTime.now());
         userRepository.save(user);
+        mailService.sendConfirmationEmail(user);
     }
 
     @Override
