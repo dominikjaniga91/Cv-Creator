436dc05ae6b167eda49bd9f83ace0dd1d130e2c8
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-08 15:26:34 +0200

    Add sendWelcomeEmail method to confirm controller

--- src/main/java/com/cvgenerator/controller/ConfirmTokenController.java
+++ src/main/java/com/cvgenerator/controller/ConfirmTokenController.java
@@ -4,6 +4,7 @@ import com.cvgenerator.domain.entity.Token;
 import com.cvgenerator.domain.entity.User;
 import com.cvgenerator.service.implementation.TokenServiceImpl;
 import com.cvgenerator.service.implementation.UserServiceImpl;
+import com.cvgenerator.utils.service.implementation.MailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,15 @@ public class ConfirmTokenController {
 
     private final TokenServiceImpl tokenService;
     private final UserServiceImpl userService;
+    private final MailServiceImpl mailService;
 
     @Autowired
     public ConfirmTokenController(TokenServiceImpl tokenService,
-                                  UserServiceImpl userService) {
+                                  UserServiceImpl userService,
+                                  MailServiceImpl mailService) {
         this.tokenService = tokenService;
         this.userService = userService;
+        this.mailService = mailService;
     }
 
     @GetMapping("/token")
@@ -37,6 +41,7 @@ public class ConfirmTokenController {
                 User user = foundedToken.getUser();
                 user.setActive(true);
                 userService.updateUser(user);
+                mailService.sendWelcomeEmail(user);
                 return new ResponseEntity<>("Your account is active", HttpStatus.OK);
             }else{
                 return new ResponseEntity<>("Confirmation link expired", HttpStatus.BAD_REQUEST);
