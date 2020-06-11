08ef69ca333cacf808bf0e1d14239b9f82bb9d77
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-11 21:14:15 +0200

    Test ConfirmTokenController with invalid token

--- src/test/java/com/cvgenerator/controller/ConfirmTokenControllerTest.java
+++ src/test/java/com/cvgenerator/controller/ConfirmTokenControllerTest.java
@@ -6,18 +6,21 @@ import com.cvgenerator.utils.service.implementation.MailServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 @DisplayName("Request to confirm controller using http method")
 public class ConfirmTokenControllerTest {
@@ -53,6 +56,17 @@ public class ConfirmTokenControllerTest {
                 .param("value", token))
                 .andExpect(status().isOk())
                 .andExpect(mvcResult -> Assertions.assertEquals(mvcResult.getResponse().getContentAsString(), "Your account is active"));
+
+    }
+
+    @Test
+    @DisplayName("should return status 'Bad request' after request")
+    void shouldReturnStatusBadRequest_afterRequest() throws Exception {
+
+        mockMvc.perform(get("/api/token")
+                .param("value", "validToken"))
+                .andExpect(status().isBadRequest())
+                .andExpect(mvcResult -> Assertions.assertEquals(mvcResult.getResponse().getContentAsString(), "Confirmation link doesn't exist"));
 
     }
 }
