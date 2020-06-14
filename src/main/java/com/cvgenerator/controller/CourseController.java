7e778ca9d8196e788853acd730ff5fcff82ee641
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-14 13:15:51 +0200

    Refactor endpoints in CourseController

--- src/main/java/com/cvgenerator/controller/CourseController.java
+++ src/main/java/com/cvgenerator/controller/CourseController.java
@@ -4,12 +4,13 @@ import com.cvgenerator.domain.entity.Course;
 import com.cvgenerator.service.implementation.CourseServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "Course controller")
 @RestController
-@RequestMapping("/api/cv")
+@RequestMapping("/api/cv/course")
 public class CourseController {
 
     private final CourseServiceImpl courseService;
@@ -20,22 +21,23 @@ public class CourseController {
 
     @ApiOperation(value = "Save new course for cv with specific ID into database ")
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/course/{id}")
-    public void createCourse(@PathVariable Long id, @RequestBody Course course) {
-        courseService.createCourse(id, course);
+    @PostMapping("/{cvId}")
+    public void createCourse(@PathVariable Long cvId,
+                             @RequestBody Course course) {
+        courseService.createCourse(cvId, course);
     }
 
     @ApiOperation(value = "Updates details about user courses")
     @ResponseStatus(HttpStatus.OK)
-    @PutMapping("/course")
+    @PutMapping("")
     public void updateEducation(@RequestBody Course course) {
         courseService.updateCourse(course);
     }
 
     @ApiOperation(value = "Delete course from database")
     @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping("/course/{id}")
-    public void deleteEducation(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public void deleteEducation(@ApiParam(value = "education ID", example = "1") @PathVariable Long id) {
         courseService.deleteCourse(id);
     }
 }
