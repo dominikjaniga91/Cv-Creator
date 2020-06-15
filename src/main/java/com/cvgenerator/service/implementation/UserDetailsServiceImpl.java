f90f0290f0fe92f3a1bbe4cb14c43281bc44eca0
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-15 23:08:27 +0200

    Implement UserNotFoundException in service layer

--- src/main/java/com/cvgenerator/service/implementation/UserDetailsServiceImpl.java
+++ src/main/java/com/cvgenerator/service/implementation/UserDetailsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cvgenerator.service.implementation;
 
+import com.cvgenerator.config.Messages;
 import com.cvgenerator.domain.entity.User;
+import com.cvgenerator.exceptions.UserNotFoundException;
 import com.cvgenerator.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,13 +15,16 @@ import java.util.Optional;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final Messages messages;
 
-    public UserDetailsServiceImpl(UserRepository userRepository) {
+    public UserDetailsServiceImpl(UserRepository userRepository,
+                                  Messages messages) {
         this.userRepository = userRepository;
+        this.messages = messages;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findUserByEmail(username).orElseThrow();
+        return userRepository.findUserByEmail(username).orElseThrow(() -> new UserNotFoundException(messages.get("user.notfound")));
     }
 }
