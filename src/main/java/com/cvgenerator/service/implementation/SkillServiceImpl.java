0e2ee9414138285e6534f0df4af7673d12cb63d2
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-15 23:21:51 +0200

    Implement Exceptions in SkillServiceImpl

--- src/main/java/com/cvgenerator/service/implementation/SkillServiceImpl.java
+++ src/main/java/com/cvgenerator/service/implementation/SkillServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cvgenerator.service.implementation;
 
+import com.cvgenerator.config.Messages;
 import com.cvgenerator.domain.entity.Skill;
 import com.cvgenerator.domain.entity.UserCv;
+import com.cvgenerator.exceptions.SkillNotFoundException;
+import com.cvgenerator.exceptions.UserCvNotFoundException;
 import com.cvgenerator.repository.SkillRepository;
 import com.cvgenerator.repository.UserCvRepository;
 import com.cvgenerator.service.SkillService;
@@ -14,17 +17,21 @@ public class SkillServiceImpl implements SkillService {
 
     private final SkillRepository skillRepository;
     private final UserCvRepository userCvRepository;
+    private final Messages messages;
 
     @Autowired
-    public SkillServiceImpl(SkillRepository skillRepository, UserCvRepository userCvRepository) {
+    public SkillServiceImpl(SkillRepository skillRepository,
+                            UserCvRepository userCvRepository,
+                            Messages messages) {
         this.skillRepository = skillRepository;
         this.userCvRepository = userCvRepository;
+        this.messages = messages;
     }
 
     @Override
     public void saveSkill(Long userCvId, Skill skill) {
 
-        UserCv userCv = userCvRepository.findById(userCvId).orElseThrow();
+        UserCv userCv = userCvRepository.findById(userCvId).orElseThrow(() -> new UserCvNotFoundException(messages.get("userCv.notfound")));
         skill.setUserCv(userCv);
         skillRepository.save(skill);
     }
@@ -32,7 +39,7 @@ public class SkillServiceImpl implements SkillService {
     @Override
     public void updateSkill(Skill skill) {
         Long id = skill.getId();
-        Skill foundedSkill = skillRepository.findById(id).orElseThrow();
+        Skill foundedSkill = skillRepository.findById(id).orElseThrow(() -> new SkillNotFoundException(messages.get("skill.notfound")));
 
         foundedSkill.setName(skill.getName());
         foundedSkill.setDescription(skill.getDescription());
@@ -44,6 +51,11 @@ public class SkillServiceImpl implements SkillService {
 
     @Override
     public void deleteSkillById(Long id) {
-        skillRepository.deleteById(id);
+        try{
+            skillRepository.deleteById(id);
+        }catch (Exception ex){
+            throw new SkillNotFoundException(messages.get("skill.notfound"));
+        }
+
     }
 }
