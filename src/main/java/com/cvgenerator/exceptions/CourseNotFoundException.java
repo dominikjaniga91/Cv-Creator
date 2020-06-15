771e42837fdd6535c71e0e2d34e26d9cc8f31796
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-15 21:47:58 +0200

    Create CourseNotFoundException class

---
+++ src/main/java/com/cvgenerator/exceptions/CourseNotFoundException.java
@@ -0,0 +1,11 @@
+package com.cvgenerator.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CourseNotFoundException extends RuntimeException {
+    public CourseNotFoundException(String message) {
+        super(message);
+        log.error(message);
+    }
+}
