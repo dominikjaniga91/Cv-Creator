a11b5bc460c2518fc8da8ba13affd9aa47f19a44
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-16 11:42:34 +0200

    Implement Exceptions in ExperienceServiceImpl

--- src/main/java/com/cvgenerator/service/implementation/ExperienceServiceImpl.java
+++ src/main/java/com/cvgenerator/service/implementation/ExperienceServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cvgenerator.service.implementation;
 
+import com.cvgenerator.config.Messages;
 import com.cvgenerator.domain.entity.Experience;
 import com.cvgenerator.domain.entity.UserCv;
+import com.cvgenerator.exceptions.ExperienceNotFoundException;
+import com.cvgenerator.exceptions.UserCvNotFoundException;
 import com.cvgenerator.repository.ExperienceRepository;
 import com.cvgenerator.repository.UserCvRepository;
 import com.cvgenerator.service.ExperienceService;
@@ -13,16 +16,20 @@ public class ExperienceServiceImpl implements ExperienceService {
 
     private final UserCvRepository userCvRepository;
     private final ExperienceRepository experienceRepository;
+    private final Messages messages;
 
     @Autowired
-    public ExperienceServiceImpl(UserCvRepository userCvRepository, ExperienceRepository experienceRepository) {
+    public ExperienceServiceImpl(UserCvRepository userCvRepository,
+                                 ExperienceRepository experienceRepository,
+                                 Messages messages) {
         this.userCvRepository = userCvRepository;
         this.experienceRepository = experienceRepository;
+        this.messages = messages;
     }
 
     @Override
     public void createExperience(Long userCvId, Experience experience) {
-        UserCv userCv = userCvRepository.findById(userCvId).orElseThrow();
+        UserCv userCv = userCvRepository.findById(userCvId).orElseThrow(() -> new UserCvNotFoundException(messages.get("userCv.notfound")));
         experience.setUserCv(userCv);
         experienceRepository.save(experience);
     }
@@ -31,7 +38,7 @@ public class ExperienceServiceImpl implements ExperienceService {
     public void updateExperience(Experience experience) {
 
         Long id = experience.getId();
-        Experience foundedExperience = experienceRepository.findById(id).orElseThrow();
+        Experience foundedExperience = experienceRepository.findById(id).orElseThrow(() -> new ExperienceNotFoundException(messages.get("experience.notfound")));
 
         foundedExperience.setId(experience.getId());
         foundedExperience.setCompany(experience.getCompany());
@@ -46,6 +53,10 @@ public class ExperienceServiceImpl implements ExperienceService {
 
     @Override
     public void deleteExperienceById(Long id) {
-        experienceRepository.deleteById(id);
+        try{
+            experienceRepository.deleteById(id);
+        }catch (Exception ex){
+            throw new ExperienceNotFoundException(messages.get("experience.notfound"));
+        }
     }
 }
