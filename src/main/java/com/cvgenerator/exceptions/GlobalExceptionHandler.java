3644c384a4cb83291dcbfabaae263413432eda54
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-30 14:30:39 +0200

    Override handleMissingServletRequestParameter

--- src/main/java/com/cvgenerator/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/cvgenerator/exceptions/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -27,6 +28,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorMessage errorMessage = getErrorMessage(status, ex);
+        return new ResponseEntity<>(errorMessage, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         ErrorMessage errorMessage = getErrorMessage(status, ex);
         return new ResponseEntity<>(errorMessage, status);
     }
@@ -143,6 +150,4 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         errorMessage.setErrorMessage("2 Factor authorization is temporary unavailable. Please contact our customer service");
         return new ResponseEntity<>(errorMessage, HttpStatus.UNAUTHORIZED);
     }
-
-
 }
