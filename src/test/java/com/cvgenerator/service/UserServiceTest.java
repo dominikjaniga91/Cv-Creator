5413b987bf6a04d47c3c6df54c1d4544402246d6
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-07-02 19:07:44 +0200

    Create test for get users from database

---
+++ src/test/java/com/cvgenerator/service/UserServiceTest.java
@@ -0,0 +1,52 @@
+package com.cvgenerator.service;
+
+import com.cvgenerator.domain.entity.User;
+import com.cvgenerator.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@DisplayName("Integration test with database should return")
+public class UserServiceTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+
+        user = new User.UserBuilder()
+                .setId(1L)
+                .setFirstName("Dominik")
+                .setLastName("Janiga")
+                .setEmail("dominikjaniga91@gmail.com")
+                .setRole("USER")
+                .setActive(true)
+                .buildUserDto();
+
+        userRepository.save(user);
+    }
+
+    @Test
+    @Transactional
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    @DisplayName("user with name 'Dominik' ")
+    void shouldReturnOneUser_afterSaveUserToDatabase(){
+        String expected = "Dominik";
+        List<User> users = userRepository.findAll();
+        User foundedUser = users.get(0);
+
+        Assertions.assertEquals(1, users.size());
+        Assertions.assertEquals(expected, foundedUser.getFirstName());
+    }
+}
