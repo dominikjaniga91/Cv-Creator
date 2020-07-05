082b68951ad2232e8dd541ffb2c1a9557d9eafb4
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-07-05 09:29:43 +0200

    Add test for new password method

--- src/test/java/com/cvgenerator/service/UserServiceTest.java
+++ src/test/java/com/cvgenerator/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.cvgenerator.service;
 import com.cvgenerator.domain.dto.UserDto;
 import com.cvgenerator.domain.entity.User;
 import com.cvgenerator.exceptions.notfound.UserNotFoundException;
+import com.cvgenerator.service.dtoConverters.UserDtoConverter;
 import com.cvgenerator.service.implementation.UserServiceImpl;
 import com.cvgenerator.utils.service.implementation.MailServiceImpl;
 import org.junit.jupiter.api.*;
@@ -12,6 +13,7 @@ import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -23,8 +25,9 @@ import javax.transaction.Transactional;
 @DisplayName("User service test should return")
 public class UserServiceTest {
 
-    @Autowired
-    private UserServiceImpl userService;
+    @Autowired private UserServiceImpl userService;
+    @Autowired private UserDtoConverter converter;
+    @Autowired private PasswordEncoder passwordEncoder;
 
     @MockBean
     private MailServiceImpl mailService;
@@ -67,5 +70,17 @@ public class UserServiceTest {
     void shouldThrownException_afterDeleteUserFromDatabase(){
         userService.deleteUserAccount(1L, "dominik123");
         Assertions.assertThrows(UserNotFoundException.class, () -> userService.findUserById(1L));
+    }
+
+    @Test
+    @Transactional
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    @DisplayName("new password after change")
+    void shouldReturnNewPassword_afterChange(){
+        User user = converter.convertToEntity(userDto);
+        userService.updateUserPassword(user, "dominik1234");
+        UserDto foundedUser = userService.findUserById(1L);
+
+        Assertions.assertTrue(passwordEncoder.matches("dominik1234", foundedUser.getPassword()));
     }
 }
