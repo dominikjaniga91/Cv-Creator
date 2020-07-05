337763ffc192262806c6a56499e6010c023b2cd7
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-07-05 10:05:19 +0200

    Create UserCvServiceTest with test for get method

---
+++ src/test/java/com/cvgenerator/service/UserCvServiceTest.java
@@ -0,0 +1,75 @@
+package com.cvgenerator.service;
+
+import com.cvgenerator.domain.dto.UserCvDto;
+import com.cvgenerator.domain.dto.UserDto;
+import com.cvgenerator.domain.entity.User;
+import com.cvgenerator.service.dtoConverters.UserCvDtoConverter;
+import com.cvgenerator.service.implementation.UserCvServiceImpl;
+import com.cvgenerator.service.implementation.UserServiceImpl;
+import com.cvgenerator.utils.service.MailService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DisplayName("User CV service test should return")
+public class UserCvServiceTest {
+
+    @Autowired private UserServiceImpl userService;
+    @Autowired private UserCvServiceImpl userCvService;
+    @Autowired private UserCvDtoConverter converter;
+    @MockBean private MailService mailService;
+    private UserDto userDto;
+    private UserCvDto userCvDto;
+
+    @BeforeEach
+    void setUp() {
+
+        BDDMockito.doNothing().when(mailService).sendConfirmationEmail(ArgumentMatchers.any(User.class));
+
+        userDto = new UserDto.UserDtoBuilder()
+                .setId(1L)
+                .setFirstName("Dominik")
+                .setLastName("Janiga")
+                .setEmail("dominikjaniga91@gmail.com")
+                .setPassword("dominik123")
+                .setRole("USER")
+                .setActive(true)
+                .buildUserDto();
+
+        userService.saveUser(userDto);
+
+        userCvDto = new UserCvDto.UserCvDtoBuilder()
+                .setId(1L)
+                .setName("Dominik cv")
+                .setTemplateName("Aquarius")
+                .buildUserCvDto();
+
+        userCvService.saveUserCv(1L, userCvDto);
+    }
+
+    @Test
+    @Transactional
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    @DisplayName("one CV with name 'Dominik cv' ")
+    void shouldReturnOneUser_afterGetUsersFromDatabase(){
+        String expected = "Dominik cv";
+        UserCvDto userCvDto = userCvService.getUserCvById(1L);
+
+        Assertions.assertEquals(expected, userCvDto.getName());
+    }
+}
