d24119beeebea7b6eb499a411550fab0b2077fb2
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-08-17 13:10:53 +0200

    Add JsonProperty.Access.WRITE_ONLY to password

--- src/main/java/com/cvgenerator/domain/dto/UserDto.java
+++ src/main/java/com/cvgenerator/domain/dto/UserDto.java
@@ -1,7 +1,7 @@
 package com.cvgenerator.domain.dto;
 
 import com.cvgenerator.domain.enums.Country;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +20,8 @@ public class UserDto {
     private String lastName;
     private String email;
     private String role;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private boolean enable2FA;
     private boolean isActive;
